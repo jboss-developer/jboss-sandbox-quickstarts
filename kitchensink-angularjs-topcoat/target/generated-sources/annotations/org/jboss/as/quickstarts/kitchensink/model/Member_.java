ad7007c6fb719050a34f3718ad6fe511f7a5f1b5
Rafael Benevides <rafabene@gmail.com>
2014-08-26 11:45:05 -0300

    DEVELOPER-958 - Remove Picketlink quickstarts from Sandbox

--- kitchensink-angularjs-topcoat/target/generated-sources/annotations/org/jboss/as/quickstarts/kitchensink/model/Member_.java
+++ kitchensink-angularjs-topcoat/target/generated-sources/annotations/org/jboss/as/quickstarts/kitchensink/model/Member_.java
@@ -8,10 +8,10 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(Member.class)
 public abstract class Member_ {
 
-	public static volatile SingularAttribute<Member, Long> id;
 	public static volatile SingularAttribute<Member, String> phoneNumber;
-	public static volatile SingularAttribute<Member, String> email;
 	public static volatile SingularAttribute<Member, String> name;
+	public static volatile SingularAttribute<Member, Long> id;
+	public static volatile SingularAttribute<Member, String> email;
 
 }
 
